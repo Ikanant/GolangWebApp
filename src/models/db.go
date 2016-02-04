0972dea39dbac3a808fa503ae36f3565d77d1e88
Jonathan Hernandez <jonathan.hdez92@gmail.com>
2016-02-03 23:37:08 -0500

    Finished Heroku Postgres DB setup

--- src/models/db.go
+++ src/models/db.go
@@ -5,18 +5,20 @@ package models
 // We want to use the library inicialization code, but we don't need anything esle
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
-	"fmt"
+	"github.com/lib/pq"
+	"log"
 )
 
 func getDBConnection() (*sql.DB, error) {
-	
-	DB_USER := "postgres"
-	DB_PASSWORD := "apple"
-	DB_NAME := "GotrainDB"
-	
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
-	
-	db, err := sql.Open("postgres", dbinfo)
+
+	dbUrl := "postgres://smjmqpgjbdlofw:Cq0Pxs_LRU0x1XDHq4Fv1IXLBe@ec2-107-21-218-93.compute-1.amazonaws.com:5432/d9qkjrujasg2kn"
+	connection, _ := pq.ParseURL(dbUrl)
+
+	db, err := sql.Open("postgres", connection)
+
+	if err != nil {
+		log.Println(err)
+	} 
+
 	return db, err
 }
